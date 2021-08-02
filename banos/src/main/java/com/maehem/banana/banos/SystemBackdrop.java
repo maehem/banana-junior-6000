da5d0cf3e194d0f141c6a63a8fe774d72b5274a0
Mark J Koch <maehem@users.noreply.github.com>
2021-08-01 20:51:50 -0700

    Backdrop working

---
+++ banos/src/main/java/com/maehem/banana/banos/SystemBackdrop.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2021 Mark J. Koch
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.maehem.banana.banos;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author mark
+ */
+public class SystemBackdrop extends Pane {
+    Canvas canvas = new Canvas();
+    ImageView iv = new ImageView();
+    
+    Color[] COL = { Color.BLACK, Color.LIGHTGRAY };
+
+    public SystemBackdrop() {
+        getChildren().add(iv);
+    }
+    
+    
+    public void update() {
+        WritableImage wi = new WritableImage((int)getWidth(), (int)getHeight());
+        int col = 0;
+        PixelWriter pw = wi.getPixelWriter();
+        for ( int y=0; y<wi.getHeight(); y++ ) {
+            int rCol = col;
+            for ( int x=0; x<wi.getWidth(); x++ ) {
+                pw.setColor(x, y, COL[rCol]);
+                if ( x % 2 == 1 ) rCol ^= 1;
+            }
+            if ( y % 2 == 1 ) col ^= 1;  // Next row starts with opposite color
+        }
+        
+        iv.setImage(wi);
+    }
+}
--- banos/src/main/java/com/maehem/banana/banos/SystemPane.java
+++ banos/src/main/java/com/maehem/banana/banos/SystemPane.java
@@ -1,7 +1,17 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright 2021 Mark J. Koch
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.maehem.banana.banos;
 
@@ -14,12 +24,19 @@ import javafx.scene.layout.StackPane;
  * @author mark
  */
 public class SystemPane extends StackPane {
-    private final Image floppyGlyph = new Image(getClass().getResourceAsStream("/glyphs/floppy-disk.png"));
+    private final SystemBackdrop backdrop = new SystemBackdrop();
+    private final Image floppyGlyph = new Image(
+            getClass().getResourceAsStream("/glyphs/floppy-disk.png"),
+            32, 32, false, false
+    );
     
     public SystemPane() {
         
-        getChildren().add(new ImageView(floppyGlyph));
+        getChildren().addAll(backdrop, new ImageView(floppyGlyph));
     }
     
+    public void update() {
+        backdrop.update();
+    }
     
 }
