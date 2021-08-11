02eb88e02806a5ae47a3d97685fb5fe5e160bd51
Mark J Koch <maehem@users.noreply.github.com>
2021-08-11 16:39:38 +0200

    Refactor jar files

--- banos/src/main/java/com/maehem/banana/banos/system/SystemBackdrop.java
+++ banos/src/main/java/com/maehem/banana/banos/system/SystemBackdrop.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.maehem.banana.hardware.m6000;
+package com.maehem.banana.banos.system;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
--- banos/src/main/java/module-info.java
+++ banos/src/main/java/module-info.java
@@ -2,5 +2,6 @@ module com.maehem.banana.banos {
     requires java.logging;
     requires javafx.graphics;
     
+    exports com.maehem.banana.banos.system;
     exports com.maehem.banana.banos.storage;
 }
